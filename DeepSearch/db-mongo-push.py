a993dc8ca82430d248a2cb71a9258063d77a27e5
Alfredo Suarez <31414624+alfredosa@users.noreply.github.com>
2022-08-13 17:22:12 +0200

    adding latest mongo version

--- DeepSearch/db-mongo-push.py
+++ DeepSearch/db-mongo-push.py
@@ -1,15 +1,24 @@
+from typing import Container
 from realapi import csv_generator
 import pymongo
 myclient = pymongo.MongoClient("mongodb+srv://alf-deepen:GWvfeEk5w5TgcXSa@cluster0.ixkyxa7.mongodb.net/?retryWrites=true&w=majority")
 mydb = myclient["papers"]
 mycol = mydb["researchpapers"]
 
-mylist = csv_generator('brain implant injury',50)
-duplicates = 0
-for document in mylist:
-    try:
-        insertid = mycol.insert_many(document)
-    except:
-        duplicates += 1
+container = [
+    ["neuronal circuit",36695],
+    ["voltage imaging",13362],
+    ["brain imaging animal",75055]
+]
 
-print("there were ", str(duplicates), " Duplicates in the insertion")
+for item in container:
+
+    mylist = csv_generator(item[0],item[1])
+    duplicates = 0
+    for document in mylist:
+        try:
+            insertid = mycol.insert_many(document)
+        except:
+            duplicates += 1
+
+    print("there were ", str(duplicates), " Duplicates in the insertion")
