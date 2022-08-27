7d826f09fd15f4fe06cefe9b3ceb50841a104a65
Patrick Westermann <pwest.jena@gmail.com>
2022-08-27 16:21:03 +0200

    worked on the local frontend

--- DeepSearch/local_frontend.py
+++ DeepSearch/local_frontend.py
@@ -1,40 +1,64 @@
 # making a frontend that can be run locally and allows userinput"
+from lib2to3.pgen2 import token
 import streamlit as st
 import pandas as pd
-import requests
 import nlp
+import timeit
+import matplotlib.pyplot as plt
+import wordcloud
 
+st.header('Prototype Research Locator')
 #token = pd.read_csv('data/tokenized_df.csv')
 st.markdown('''
 Hey, here you can use our amazing research locator!
-First push teh button to load the most recient database from the cloud!
+First you need to wait a bit until the most recent dataset is pulled from the mongo db cloud! This might take a minute but your data will be nice and fresh ;)
 ''')
-
+#loads the dataframe when the session is started
+start = timeit.timeit()
 @st.cache(allow_output_mutation=True)
 def get_df():
-    df  = nlp.main()
-    return df
-df = get_df()
+    mydoc = nlp.get_data()
+    df = nlp.dataframe(mydoc)
+    tokenized_df = nlp.tokenize(df)
+    return df, tokenized_df
+df, tokenized_df = get_df()
+end = timeit.timeit()
+st.write(f"It took {end - start} to load the data")
 
-
-#with st.form("get DF"):
-    #submitted = st.form_submit_button("Click the button and get the DF")
-    #if submitted:
-        #df = get_df()
-        #st.markdown('''Dataframe loaded sucessfully''')'
-
-
+#let the user put in the search terms and start ranking process
 st.markdown('''
-Just put in some topics you are interested in, we take up to 5!
+Okay, you got access to our database. Now, give us some medical or neuroscience related topics you are interested and see where in the world they are researched!
 ''')
-
-search_terms = st.text_area('Put in the words')
-
+search_terms = st.text_area('Put in the words, sparate them with an empty space')
 search_term_list = list(search_terms.split(" "))
+load = st.button("Let's find the most relevant articles!")
 
-article_count = st.slider('How many articles should be displayed?', 1, 200, 1)
-df[:article_count]
 
-#list_of_results = nlp.rank(token, search_term_list)
+#initialize session state
+if "load_state" not in st.session_state:
+    st.session_state.load_state = False
+#ranking
+if load or st.session_state.load_state:
+    st.session_state.load_state = True
+    @st.cache(allow_output_mutation=True)
+    def load_ranked_df():
+        ids_ranked = nlp.rank(tokenized_df, words = search_term_list)
+        return ids_ranked
+    ids_ranked = load_ranked_df()
+    article_count = st.slider('How many articles should be displayed?', 1, 200, 1)
 
-st.map()
+   #ids_ranked[:article_count]
+    df_ranked = df.loc[ids_ranked[:article_count].index]
+    df_ranked
+
+data = df_ranked[0]['abstract']
+
+plt.subplots(figsize = (8,8))
+
+wordcloud = WordCloud (
+                    background_color = 'white',
+                    width = 512,
+                    height = 384
+                        ).generate(' '.join(data))
+plt.imshow(wordcloud) # image show
+plt.axis('off')
