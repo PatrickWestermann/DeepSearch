e10268da398e0a31e6651ec6e2a5fff6a8d6d7d0
iShapeNoise <iShapeNoise@github.com>
2022-08-29 14:42:47 +0200

    Update db module

--- DeepSearch/db_toolkit.py
+++ DeepSearch/db_toolkit.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python
-
 import os
 import sqlite3
 from sqlite3 import Error
 from sqlalchemy import create_engine
 from sqlalchemy.engine import Engine
 from sqlalchemy import event
+from sqlalchemy.exc import SQLAlchemyError
 import pandas as pd
 
 
@@ -15,6 +15,7 @@ class ToolKit:
     def __init__(self):
         print("Toolkit functions: ")
         print("     create_db(database)")
+        print("     create_tbl(engine, tbl_name)")
         print("     connect_db(database)")
         print("     disconnect_db(engine)")
         print("     csv_to_db(engine, csv_file, tbl_name)")
@@ -38,30 +39,28 @@ class ToolKit:
         """ create a database connection to a database that resides
             in the memory
         """
-        self.engine = create_engine('sqlite:///'+db, echo=False)
+        self.engine = create_engine('sqlite:////'+db, echo=False)
+        self.engine.connect()
+        print(f"Connected to database >> {db}")
         return self.engine
 
-    @event.listens_for(Engine, "connect")
-    def set_sqlite_pragma(self, engine):
-        self.engine = engine
-        self.cursor = self.engine.cursor()
-        self.cursor.execute("PRAGMA journal_mode=WAL")
-        self.cursor.execute("PRAGMA synchronous=normal")
-        self.cursor.execute("PRAGMA temp_store=memory")
-        self.cursor.execute("PRAGMA mmap_size=30000000000")
-        self.cursor.close()
-        return print("Pragma arguments added")
-
     def disconnect_db(self, engine):
-        self.engine = engine
-        self.engine.close()
+        engine.close()
+        print(f"Disconnected from database")
 
-    def csv_to_db(self, engine, csv_file, tbl_name):
+    def create_tbl(self, engine, tbl_name):
+        try:
+            self.engine.connect()
+            print("Connection successful")
+        except SQLAlchemyError as err:
+            print("error", err.__cause__)  # this will give what kind of error
+
+    def csv_to_db(self, engine, csv_file, tbl_name, on_exists='replace'):
         # creating a data frame
         self.df = pd.read_csv(csv_file)
         """Take a data frame and add it to a sqlite db"""
         self.df = self.df.applymap(str)
-        self.df.to_sql(str(tbl_name), con=engine, if_exists='replace')
+        self.df.to_sql(str(tbl_name), con=engine, if_exists=on_exists)
         engine.close()
 
     def tbl_to_df(self, db, tbl_name):
@@ -70,3 +69,13 @@ class ToolKit:
         # Read table out to data frame
         df = pd.read_sql_table(tbl_name, engine)
         return df
+
+    @event.listens_for(Engine, "connect")
+    def set_sqlite_pragma(dbapi_connection, connection_record):
+        self.cursor = self.dbapi_connection.cursor()
+        self.cursor.execute("PRAGMA journal_mode=WAL")
+        self.cursor.execute("PRAGMA synchronous=normal")
+        self.cursor.execute("PRAGMA temp_store=memory")
+        self.cursor.execute("PRAGMA mmap_size=30000000000")
+        self.cursor.close()
+        return print("Pragma arguments added")
